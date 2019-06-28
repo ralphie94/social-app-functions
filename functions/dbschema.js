48bb5e38a52a5e7b709d68cec04f768d30ef204d
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-06-28 15:10:13 -0700

    firebase index.js set up

---
+++ functions/dbschema.js
@@ -0,0 +1,11 @@
+ let db = {
+     screams: [
+         {
+             userHandle: "user",
+             body: "this is the scream body",
+             createdAt: "2019-06-28T21:56:32.683Z",
+             likeCount: 5,
+             commentCount: 2
+         }
+     ]
+};
