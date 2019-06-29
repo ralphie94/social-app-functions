325202f69a36ef38e91ec178d5cc4d87b0043fd7
Ralphie Celedon <ralphie@ralphies-mbp.attlocal.net>
2019-06-29 12:37:58 -0700

    refactored functions code

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyAZmLVxmfV2guR9oG2DkTibYCsGourM0FI",
+    authDomain: "socialapp-b3125.firebaseapp.com",
+    databaseURL: "https://socialapp-b3125.firebaseio.com",
+    projectId: "socialapp-b3125",
+    storageBucket: "socialapp-b3125.appspot.com",
+    messagingSenderId: "637629553681",
+    appId: "1:637629553681:web:33992a4a3f11924a"
+  };
