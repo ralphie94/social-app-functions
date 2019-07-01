1adbc2c4b03debb52ef34308a1d3fee3153de0f0
Ralphie Celedon <ralphie@Ralphies-MacBook-Pro.local>
2019-07-01 11:03:47 -0700

    added route to get user credentials

--- functions/dbschema.js
+++ functions/dbschema.js
@@ -9,3 +9,26 @@
          }
      ]
 };
+
+const userDetails = {
+    credentials: {
+        userId: "MQsek5Ll4xRlyyq1kYBkRs2ohhg1",
+        email: "new2@email.com",
+        handle: "new2",
+        createdAt: "2019-06-29T20:22:21.721Z",
+        imageUrl: "https://firebasestorage.googleapis.com/v0/b/socialapp-b3125.appspot.com/o/8950044879.jpg?alt=media",
+        bio: "Hello I'm new new",
+        website: "https://user.com",
+        location: "Los Angeles, CA"
+    },
+    likes: [
+        {
+            userHandle: "user",
+            screamId: "1crpsliQRLqISV6V3Yvt"
+        },
+        {
+            userHandle: "new",
+            screamId: "vawK776Z4GuSJZ9Z7qE5"
+        }
+    ]
+};
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users")
+const { signup, login, uploadImage, addUserDetails, getAuthenticatedUser } = require("./handlers/users")
 
 const express = require("express");
 const app = express();
@@ -11,6 +11,8 @@ const FBAuth = require("./util/fbAuth")
 // Scream Routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 // Users routes
 app.post("/signup", signup);
